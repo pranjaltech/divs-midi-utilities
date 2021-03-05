34771214e5064c1e68acae144fe03b8ee5da3b28
Pranjal Singh <pranjal.singh@turtlemint.com>
2021-03-05 11:08:56 +0000

    Cleanup Pi after transferring midi session.

--- after_save.sh
+++ after_save.sh
@@ -11,7 +11,7 @@ curl --header "Content-Type: application/json" \
   --data '{"username": "midi-helper", "content": "MIDI File Captured. Saving... ${filename}"}' \
   https://discord.com/api/webhooks/817323024348610611/3WiDRgTUXSe4mEE02gSecu0D6dIIy1hoaB9nJQ3y8df8140EJv0F647bKZGXb8z3Q7SX
 
-rsync -azP $HOME/midi-session-data pi@192.168.1.35:/home/pi/Titan/midi-session-data
+rsync -azP --remove-source-files $HOME/midi-session-data/ pi@192.168.1.35:/home/pi/Titan/midi-session-data
 
 curl --header "Content-Type: application/json" \
   --request POST \
