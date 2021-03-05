e62216686472e71202ead4c7ef88d7e6047a75c0
Pranjal Singh <pranjal.singh@turtlemint.com>
2021-03-05 16:23:22 +0000

    Some fine tuning

--- after_save.sh
+++ after_save.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-while getopts u:a:f: flag
+while getopts f: flag
 do
 	case "${flag}" in
 		f) filename=${OPTARG};;
 	esac
 done
 
+echo "Saving" $filename
+
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"username": "midi-helper", "content": "MIDI File Captured. Saving... ${filename}"}' \
+  --data '{"username": "midi-helper", "content": "MIDI File Captured. Saving... '${filename}'"}' \
   https://discord.com/api/webhooks/817323024348610611/3WiDRgTUXSe4mEE02gSecu0D6dIIy1hoaB9nJQ3y8df8140EJv0F647bKZGXb8z3Q7SX
 
 rsync -azP --remove-source-files $HOME/midi-session-data/ pi@192.168.1.35:/home/pi/Titan/midi-session-data
 
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"username": "midi-helper", "content": "MIDI File Saved: ${filename}"}' \
+  --data '{"username": "midi-helper", "content": "MIDI File Saved: '${filename}'"}' \
   https://discord.com/api/webhooks/817323024348610611/3WiDRgTUXSe4mEE02gSecu0D6dIIy1hoaB9nJQ3y8df8140EJv0F647bKZGXb8z3Q7SX
--- run-brainstorm.sh
+++ run-brainstorm.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# Guess if this is going to succeed
+/home/ubuntu/divs-midi-utilities/bin/lsmidiins | grep "1 " >/dev/null && echo "MIDI Interface exists" || sleep 1
+
 /home/ubuntu/divs-midi-utilities/bin/brainstorm --in 1 --prefix session_ --timeout 10 --confirmation "/home/ubuntu/midi-utils/after_save.sh -f %s"
