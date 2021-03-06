330d6bed07d26f1e27de920e997657f62483ebcd
Pranjal Singh <pranjal.singh@turtlemint.com>
2021-03-06 09:37:44 +0000

    Changed prefix to match midi device being used. For Reface will be used too.

--- run-brainstorm.sh
+++ run-brainstorm.sh
@@ -3,4 +3,4 @@
 # Guess if this is going to succeed
 /home/ubuntu/divs-midi-utilities/bin/lsmidiins | grep "1 " >/dev/null && echo "MIDI Interface exists" || sleep 1
 
-/home/ubuntu/divs-midi-utilities/bin/brainstorm --in 1 --prefix session_ --timeout 10 --confirmation "/home/ubuntu/midi-utils/after_save.sh -f %s"
+/home/ubuntu/divs-midi-utilities/bin/brainstorm --in 1 --prefix kronos_ --timeout 10 --confirmation "/home/ubuntu/midi-utils/after_save.sh -f %s"
